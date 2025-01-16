3f4fb55ae0899b6e3749dcf34093df867628dee0
YouChristina <youchris@usc.edu>
2025-01-16 15:40:05 -0800

    Initial commit for backend (Spring Boot) and frontend (Angular/Node) set up

---
+++ backend/src/main/java/com/mutualfundcalculator/MutualFundCalculatorApplication.java
@@ -0,0 +1,15 @@
+package com.mutualfund.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class MutualFundCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutualFundCalculatorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mutualfund/calculator/MutualFundCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mutualfund.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutualFundCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
